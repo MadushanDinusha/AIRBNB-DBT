3775d2e3d6ee5b4daf7ab839f0452422aff516b0
unknown <djabc.pwc@gmail.com>
2023-02-19 18:28:03 +0530

    initiating the project

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized = 'view',
+    )
+}}
+
+WITH src_hosts AS (
+ SELECT
+ *
+ FROM
+ {{ ref('src_hostings') }}
+)
+SELECT
+ id,
+ NVL(
+ name,
+ 'Anonymous'
+ ) AS host_name,
+ is_superhost,
+ created_at,
+ updated_at
+FROM
+ src_hosts
---
+++ models/src/src_hostings.sql
@@ -0,0 +1,5 @@
+	WITH raw_hostings AS (
+    	SELECT * from AIRBNB.RAW.RAW_HOSTS
+)
+    SELECT * 
+    FROM raw_hostings
